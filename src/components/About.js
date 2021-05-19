be6e6bbad3c566cc700222d51e247312bb599ff3
iankumu <ikariuki741>
2021-05-19 16:33:13 +0300

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+const About = () => {
+    return (
+        <h4>
+            Version 1.0.0
+            <p><Link to = '/'>Go Back</Link></p>
+        </h4>
+    )
+}
+
+export default About
